9c22a613d0cec5972ae2c1d668a6fea965fd0b3b
Adam Szkoda <adaszko@gmail.com>
2023-03-20 12:22:08 +0100

    Produce a smaller bash script

--- src/bash.rs
+++ src/bash.rs
@@ -11,43 +11,30 @@ fn write_dfa_state_function<W: Write>(buffer: &mut W, dfa: &DFA, state: StateId)
         Input::Any => None,
     }).collect();
 
-    write!(buffer, r#"
-_state_{state} () {{
-    local index=$1
-    if [[ $index == $COMP_CWORD ]]; then
+    write!(buffer, r#"_state_{state} () {{
+    local i=$1
+    if [[ $i == $COMP_CWORD ]]; then
 "#, state = state)?;
 
     // We're generating completions for the current state
     if !discriminating_inputs.is_empty() {
-        write!(buffer, r#"
-        COMPREPLY=($(compgen -W '{inputs}' -- "${{COMP_WORDS[$COMP_CWORD]}}"))
-"#, inputs = discriminating_inputs.join(" "))?;
+        writeln!(buffer, r#"        COMPREPLY=($(compgen -W '{inputs}' -- "${{COMP_WORDS[$COMP_CWORD]}}"))"#, inputs = discriminating_inputs.join(" "))?;
     }
 
-    write!(buffer, r#"
-        return 0
+    write!(buffer, r#"        return 0
     fi
 "#)?;
 
-    // We're matching $COMP_WORDS[$index] against the DFA
-    write!(buffer, r#"
-    case ${{COMP_WORDS[$index]}} in
-"#)?;
+    // We're matching $COMP_WORDS[$i] against the DFA
+    writeln!(buffer, r#"    case ${{COMP_WORDS[$i]}} in"#)?;
 
     // XXX Input::Any should be the last case branch to use it as a fallback when everything more
     // specific failed
     for (input, to) in dfa.get_transitions_from(state) {
-        write!(buffer, r#"
-        {input})
-            _state_{to} $((index + 1))
-            return
-            ;;
-"#, input = input)?;
+        writeln!(buffer, r#"        {input}) _state_{to} $((i + 1)); return;;"#, input = input)?;
     }
 
-    write!(buffer, r#"
-    esac
-
+    write!(buffer, r#"    esac
     return 1
 }}
 
@@ -66,7 +53,7 @@ pub fn write_completion_script<W: Write>(buffer: &mut W, command: &str, dfa: &DF
 
     write!(buffer, r#"
 _{command}_completions () {{
-  _state_{start_state} 1
+    _state_{start_state} 1
 }}
 
 complete -F _{command}_completions {command}
