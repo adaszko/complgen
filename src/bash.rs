141017dddb40778b0e7ee002f4f6bec06557b6db
Adam Szkoda <adaszko@gmail.com>
2023-03-20 12:33:00 +0100

    Factor out state name construction

--- src/bash.rs
+++ src/bash.rs
@@ -5,16 +5,21 @@ use crate::dfa::DFA;
 use crate::nfa::Input;
 
 
-fn write_dfa_state_function<W: Write>(buffer: &mut W, dfa: &DFA, state: StateId) -> Result<()> {
+fn make_state_name(command: &str, state: StateId) -> String {
+    format!("_{command}_{state}", command = command, state = state)
+}
+
+
+fn write_dfa_state_function<W: Write>(buffer: &mut W, command: &str, dfa: &DFA, state: StateId) -> Result<()> {
     let discriminating_inputs: Vec<String> = dfa.get_transitions_from(state).into_iter().filter_map(|(input, _)| match input {
         Input::Literal(s) => Some(s.to_string()),
         Input::Any => None,
     }).collect();
 
-    write!(buffer, r#"_state_{state} () {{
+    write!(buffer, r#"{state_name} () {{
     local i=$1
     if [[ $i == $COMP_CWORD ]]; then
-"#, state = state)?;
+"#, state_name = make_state_name(command, state))?;
 
     // We're generating completions for the current state
     if !discriminating_inputs.is_empty() {
@@ -31,7 +36,7 @@ fn write_dfa_state_function<W: Write>(buffer: &mut W, dfa: &DFA, state: StateId)
     // XXX Input::Any should be the last case branch to use it as a fallback when everything more
     // specific failed
     for (input, to) in dfa.get_transitions_from(state) {
-        writeln!(buffer, r#"        {input}) _state_{to} $((i + 1)); return;;"#, input = input)?;
+        writeln!(buffer, r#"        {input}) {state_name} $((i + 1)); return;;"#, state_name = make_state_name(command, to), input = input)?;
     }
 
     write!(buffer, r#"    esac
@@ -45,19 +50,19 @@ fn write_dfa_state_function<W: Write>(buffer: &mut W, dfa: &DFA, state: StateId)
 
 pub fn write_completion_script<W: Write>(buffer: &mut W, command: &str, dfa: &DFA) -> Result<()> {
     for state in dfa.get_all_states() {
-        write_dfa_state_function(buffer, dfa, state)?;
+        write_dfa_state_function(buffer, command, dfa, state)?;
     }
 
     // XXX Should be exactly one, not many
     let start_state = dfa.start_states.iter().next().unwrap();
 
     write!(buffer, r#"
-_{command}_completions () {{
-    _state_{start_state} 1
+_{command} () {{
+    {state_name} 1
 }}
 
-complete -F _{command}_completions {command}
-"#, command = command)?;
+complete -F _{command} {command}
+"#, state_name = make_state_name(command, start_state), command = command)?;
 
     Ok(())
 }
