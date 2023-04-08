5d02ab4ff7501d79628709ae505d85b7534e0885
Adam Szkoda <adaszko@gmail.com>
2023-04-08 10:44:29 +0200

    Level DFA transition table in the fish output script

--- src/fish.rs
+++ src/fish.rs
@@ -6,74 +6,79 @@ use crate::nfa::Input;
 
 
 // TODO https://stackoverflow.com/a/40019138
+// TODO Renumber DFA states to save on fish shell memory
 
 
-fn make_state_name(command: &str, state: StateId) -> String {
-    format!("_{command}_{state}", command = command, state = state)
-}
-
-
-fn write_dfa_state_function<W: Write>(buffer: &mut W, command: &str, dfa: &DFA, state: StateId) -> Result<()> {
-    let mut discriminating_inputs: Vec<String> = dfa.get_transitions_from(state).into_iter().filter_map(|(input, _)| match input {
-        Input::Literal(s) => Some(s.to_string()),
-        Input::Any => None,
-    }).collect();
-    discriminating_inputs.sort();
-
-    write!(buffer, r#"function {state_name}
-    set i $argv[1]
-    if test $i -eq $COMP_CWORD
-"#, state_name = make_state_name(command, state))?;
-
-    // We're generating completions for the current state
-    if !discriminating_inputs.is_empty() {
-        writeln!(buffer, r#"        echo "{inputs}""#, inputs = discriminating_inputs.join("\n"))?;
+fn write_tables<W: Write>(buffer: &mut W, dfa: &DFA) -> Result<()> {
+    for state in dfa.get_all_states() {
+        let transitions: Vec<(Input, StateId)> = dfa.get_transitions_from(state as StateId).into_iter().filter(|(input, _)| !input.is_any()).collect();
+        if transitions.is_empty() {
+            continue;
+        }
+        let state_transitions: String = itertools::join(transitions.into_iter().map(|(input, to)| format!("set -a inputs {}; set -a tos {};", input, to+1)), " ");
+        writeln!(buffer, r#"    set transitions[{}] "set -l inputs; set -l tos; {state_transitions}""#, state+1)?;
     }
 
-    write!(buffer, r#"        return 0
-    end
-"#)?;
+    writeln!(buffer, "")?;
 
-    // We're matching $COMP_WORDS[$i] against the DFA
-    writeln!(buffer, r#"    switch $COMP_WORDS[$i]"#)?;
+    let asterisk_transitions = dfa.get_asterisk_transitions();
+    let asterisk_transitions_from = itertools::join(asterisk_transitions.iter().map(|(from, _)| format!("{}", from+1)), " ");
+    writeln!(buffer, r#"    set --local asterisk_transitions_from {asterisk_transitions_from}"#)?;
+    let asterisk_transitions_to = itertools::join(asterisk_transitions.iter().map(|(_, to)| format!("{}", to+1)), " ");
+    writeln!(buffer, r#"    set --local asterisk_transitions_to {asterisk_transitions_to}"#)?;
 
-    let mut transitions: Vec<(Input, StateId)> = dfa.get_transitions_from(state).into_iter().collect();
-    transitions.sort_by_key(|(input, _)| input.clone());
-    for (input, to) in transitions {
-        writeln!(buffer, r#"        case '{input}'
-            {state_name} (math $i + 1)
-            return 0"#, state_name = make_state_name(command, to), input = input)?;
-    }
-
-    write!(buffer, r#"    end
-    return 1
-end
-
-"#)?;
     Ok(())
 }
 
 
 pub fn write_completion_script<W: Write>(buffer: &mut W, command: &str, dfa: &DFA) -> Result<()> {
-    for state in dfa.get_all_states() {
-        write_dfa_state_function(buffer, command, dfa, state as StateId)?;
-    }
-
     write!(buffer, r#"
 function _{command}
     set COMP_LINE (commandline --cut-at-cursor)
-    set --global COMP_WORDS
+    set COMP_WORDS
     echo $COMP_LINE | read --tokenize --array COMP_WORDS
     if string match --quiet --regex '.*\s$' $COMP_LINE
-        set --global COMP_CWORD (math (count $COMP_WORDS) + 1)
+        set COMP_CWORD (math (count $COMP_WORDS) + 1)
     else
-        set --global COMP_CWORD (count $COMP_WORDS)
+        set COMP_CWORD (count $COMP_WORDS)
     end
-    {state_name} 2
+"#, command = command)?;
+
+    write_tables(buffer, dfa)?;
+
+    write!(buffer, r#"
+    set --local state {starting_state}
+    set --local word_index 2
+    while test $word_index -lt $COMP_CWORD
+        set --query transitions[$state] || return 1
+        eval $transitions[$state]
+        set --local -- word $COMP_WORDS[$word_index]
+        if contains -- $word $inputs
+            set --local index (contains --index -- $word $inputs)
+            set state $tos[$index]
+            set word_index (math $word_index + 1)
+            continue
+        end
+        if set --query asterisk_transitions_from[$state]
+            set --local index (contains --index -- $state $asterisk_transitions_from)
+            set state $asterisk_transitions_to[$index]
+            set word_index (math $word_index + 1)
+            continue
+        end
+        return 1
+    end
+
+    set --query transitions[$state] || return 1
+    eval $transitions[$state]
+    printf '%s\n' $inputs
+    return 0
+"#, starting_state = dfa.starting_state + 1)?;
+
+    write!(buffer, r#"
 end
 
 complete --command {command} --no-files --arguments "(_{command})"
-"#, state_name = make_state_name(command, dfa.starting_state), command = command)?;
+"#, command = command)?;
 
     Ok(())
 }
