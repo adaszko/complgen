e96a36374d39613e5f791255aee4f453a6885d70
Adam Szkoda <adaszko@gmail.com>
2023-09-12 09:22:29 +0200

    scraper: Handle more cases

--- src/scrape.rs
+++ src/scrape.rs
@@ -3,16 +3,37 @@ use regex::Regex;
 
 
 pub fn scrape<W: Write>(input: &str, output: &mut W) -> crate::Result<()> {
+    let short_long_opt_triangle_arg_descr = Regex::new(r#"^\s*(-\S),\s*(--\S+) \[<([^>]+)>\]\s+(.+)$"#).unwrap();
+    let short_long_triangle_arg_descr = Regex::new(r#"^\s*(-\S),\s*(--\S+) <([^>]+)>\s+(.+)$"#).unwrap();
     let short_long_arg_descr = Regex::new(r#"^\s*(-\S),\s*(--\S+)=(\p{Uppercase}+)\s+(.+)$"#).unwrap();
     let short_long_descr = Regex::new(r#"^\s*(-\S),\s*(--\S+)\s+(.+)$"#).unwrap();
     let long_arg_descr = Regex::new(r#"^\s*(--\S+)=(\p{Uppercase}+)\s+(.+)$"#).unwrap();
     let long_triangle_arg_descr = Regex::new(r#"^\s*(--\S+) <([^>]+)>\s+(.+)$"#).unwrap();
+    let long_opt_triangle_arg_descr = Regex::new(r#"^\s*(--\S+) \[<([^>]+)>\]\s+(.+)$"#).unwrap();
     let long_opt_arg_descr = Regex::new(r#"^\s*(--\S+)\[=(\p{Uppercase}+)\]\s+(.+)$"#).unwrap();
     let long_descr = Regex::new(r#"^\s*(--\S+)\s+(.+)$"#).unwrap();
     let short_triangle_arg_descr = Regex::new(r#"^\s*(-\S)\s*<([^>]+)>\s+(.+)$"#).unwrap();
     let short_descr = Regex::new(r#"^\s*(-\S)\s+(.+)$"#).unwrap();
 
     for line in input.lines() {
+        if let Some(caps) = short_long_opt_triangle_arg_descr.captures(line) {
+            let short = caps.get(1).unwrap().as_str();
+            let long = caps.get(2).unwrap().as_str();
+            let arg = format!("<{}>", caps.get(3).unwrap().as_str());
+            let descr = caps.get(4).unwrap().as_str();
+            writeln!(output, r#" | ({short} [{arg}] | {long} [{arg}]) "{descr}""#)?;
+            continue;
+        }
+
+        if let Some(caps) = short_long_triangle_arg_descr.captures(line) {
+            let short = caps.get(1).unwrap().as_str();
+            let long = caps.get(2).unwrap().as_str();
+            let arg = format!("<{}>", caps.get(3).unwrap().as_str());
+            let descr = caps.get(4).unwrap().as_str();
+            writeln!(output, r#" | ({short} {arg} | {long} {arg}) "{descr}""#)?;
+            continue;
+        }
+
         if let Some(caps) = short_long_arg_descr.captures(line) {
             let short = caps.get(1).unwrap().as_str();
             let long = caps.get(2).unwrap().as_str();
@@ -35,6 +56,14 @@ pub fn scrape<W: Write>(input: &str, output: &mut W) -> crate::Result<()> {
             let arg = format!("<{}>", caps.get(2).unwrap().as_str());
             let descr = caps.get(3).unwrap().as_str();
             writeln!(output, r#" | {long}={arg} "{descr}""#)?;
+            continue;
+        }
+
+        if let Some(caps) = long_opt_triangle_arg_descr.captures(line) {
+            let long = caps.get(1).unwrap().as_str();
+            let arg = format!("<{}>", caps.get(2).unwrap().as_str());
+            let descr = caps.get(3).unwrap().as_str();
+            writeln!(output, r#" | ({long} [{arg}]) "{descr}""#)?;
             continue;
         }
 
@@ -128,6 +157,30 @@ mod tests {
         const INPUT: &str = "      --explain <CODE>      Run `rustc --explain CODE`";
         let output = do_scrape(INPUT);
         assert_eq!(output, r#" | (--explain <CODE>) "Run `rustc --explain CODE`"
+"#);
+    }
+
+    #[test]
+    fn long_opt_triangle_arg_descr() {
+        const INPUT: &str = "      --bin [<NAME>]            Build only the specified binary";
+        let output = do_scrape(INPUT);
+        assert_eq!(output, r#" | (--bin [<NAME>]) "Build only the specified binary"
+"#);
+    }
+
+    #[test]
+    fn short_long_opt_triangle_arg_descr() {
+        const INPUT: &str = "  -p, --package [<SPEC>]        Package to build (see `cargo help pkgid`)";
+        let output = do_scrape(INPUT);
+        assert_eq!(output, r#" | (-p [<SPEC>] | --package [<SPEC>]) "Package to build (see `cargo help pkgid`)"
+"#);
+    }
+
+    #[test]
+    fn short_long_triangle_arg_descr() {
+        const INPUT: &str = "  -j, --jobs <N>                Number of parallel jobs, defaults to # of CPUs.";
+        let output = do_scrape(INPUT);
+        assert_eq!(output, r#" | (-j <N> | --jobs <N>) "Number of parallel jobs, defaults to # of CPUs."
 "#);
     }
 }
