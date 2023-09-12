443d2e933f348b64b03803ce71e6db258f109842
Adam Szkoda <adaszko@gmail.com>
2023-09-12 08:58:46 +0200

    scraper: Handle more cases

--- src/scrape.rs
+++ src/scrape.rs
@@ -6,7 +6,10 @@ pub fn scrape<W: Write>(input: &str, output: &mut W) -> crate::Result<()> {
     let short_long_arg_descr = Regex::new(r#"^\s*(-\S),\s*(--\S+)=(\p{Uppercase}+)\s+(.+)$"#).unwrap();
     let short_long_descr = Regex::new(r#"^\s*(-\S),\s*(--\S+)\s+(.+)$"#).unwrap();
     let long_arg_descr = Regex::new(r#"^\s*(--\S+)=(\p{Uppercase}+)\s+(.+)$"#).unwrap();
+    let long_triangle_arg_descr = Regex::new(r#"^\s*(--\S+) <([^>]+)>\s+(.+)$"#).unwrap();
     let long_opt_arg_descr = Regex::new(r#"^\s*(--\S+)\[=(\p{Uppercase}+)\]\s+(.+)$"#).unwrap();
+    let long_descr = Regex::new(r#"^\s*(--\S+)\s+(.+)$"#).unwrap();
+    let short_triangle_arg_descr = Regex::new(r#"^\s*(-\S)\s*<([^>]+)>\s+(.+)$"#).unwrap();
     let short_descr = Regex::new(r#"^\s*(-\S)\s+(.+)$"#).unwrap();
 
     for line in input.lines() {
@@ -35,11 +38,34 @@ pub fn scrape<W: Write>(input: &str, output: &mut W) -> crate::Result<()> {
             continue;
         }
 
+        if let Some(caps) = long_triangle_arg_descr.captures(line) {
+            let long = caps.get(1).unwrap().as_str();
+            let arg = format!("<{}>", caps.get(2).unwrap().as_str());
+            let descr = caps.get(3).unwrap().as_str();
+            writeln!(output, r#" | ({long} {arg}) "{descr}""#)?;
+            continue;
+        }
+
         if let Some(caps) = long_opt_arg_descr.captures(line) {
             let long = caps.get(1).unwrap().as_str();
             let arg = format!("<{}>", caps.get(2).unwrap().as_str());
             let descr = caps.get(3).unwrap().as_str();
             writeln!(output, r#" | {long}[={arg}] "{descr}""#)?;
+            continue;
+        }
+
+        if let Some(caps) = long_descr.captures(line) {
+            let long = caps.get(1).unwrap().as_str();
+            let descr = caps.get(2).unwrap().as_str();
+            writeln!(output, r#" | {long} "{descr}""#)?;
+            continue;
+        }
+
+        if let Some(caps) = short_triangle_arg_descr.captures(line) {
+            let short = caps.get(1).unwrap().as_str();
+            let arg = format!("<{}>", caps.get(2).unwrap().as_str());
+            let descr = caps.get(3).unwrap().as_str();
+            writeln!(output, r#" | ({short} {arg}) "{descr}""#)?;
             continue;
         }
 
@@ -78,6 +104,30 @@ mod tests {
         const INPUT: &str = r#"-e, --regexp=PATTERNS     use PATTERNS for matching"#;
         let output = do_scrape(INPUT);
         assert_eq!(output, r#" | (-e <PATTERNS> | --regexp=<PATTERNS>) "use PATTERNS for matching"
+"#);
+    }
+
+    #[test]
+    fn long_descr() {
+        const INPUT: &str = "--list                List installed commands";
+        let output = do_scrape(INPUT);
+        assert_eq!(output, r#" | --list "List installed commands"
+"#);
+    }
+
+    #[test]
+    fn short_triangle_arg_descr() {
+        const INPUT: &str = "  -C <DIRECTORY>            Change to DIRECTORY before doing anything (nightly-only)";
+        let output = do_scrape(INPUT);
+        assert_eq!(output, r#" | (-C <DIRECTORY>) "Change to DIRECTORY before doing anything (nightly-only)"
+"#);
+    }
+
+    #[test]
+    fn long_triangle_arg_descr() {
+        const INPUT: &str = "      --explain <CODE>      Run `rustc --explain CODE`";
+        let output = do_scrape(INPUT);
+        assert_eq!(output, r#" | (--explain <CODE>) "Run `rustc --explain CODE`"
 "#);
     }
 }
