ef34b4bb4b44d9102ad5bfb721ff384da89ed9bf
Adam Szkoda <adaszko@gmail.com>
2024-06-15 13:43:25 +0200

    Fix Rust code being recompiled on e2e code change

--- build.rs
+++ build.rs
@@ -5,4 +5,5 @@ fn main() {
     let output = Command::new("git").args(&["describe", "--long", "--dirty", "--abbrev=10", "--tags", "--always"]).output().unwrap();
     let version = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=COMPLGEN_VERSION={}", version);
+    println!("cargo:rerun-if-changed=.git/HEAD");
 }
