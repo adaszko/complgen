a821a85976d0c698fcb4c103c67e5be6f4ce2aea
Adam Szkoda <adaszko@gmail.com>
2024-07-07 11:54:37 +0200

    Fix #52

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
-// build.rs
 use std::process::Command;
+
 fn main() {
-    // note: add error checking yourself.
-    let output = Command::new("git").args(&["describe", "--long", "--dirty", "--abbrev=10", "--tags", "--always"]).output().unwrap();
+    let Ok(output) = Command::new("git").args(&["describe", "--long", "--dirty", "--abbrev=10", "--tags", "--always"]).output() else {
+        println!("cargo:warning=Git binary not found.  `complgen version` output will be inaccurate");
+        println!("cargo:rustc-env=COMPLGEN_VERSION={}", "fallback");
+        return;
+    };
     let version = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=COMPLGEN_VERSION={}", version);
     println!("cargo:rerun-if-changed=.git/HEAD");
