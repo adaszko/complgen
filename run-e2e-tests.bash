42c9c0a2a534bfad185b2ed329c68dbd947dfd8c
Adam Szkoda <adaszko@gmail.com>
2026-01-17 14:51:55 +0100

    Make e2e tests runnable from any directory

--- run-e2e-tests.bash
+++ run-e2e-tests.bash
@@ -7,7 +7,9 @@ set -o pipefail
 main () {
     local this_script_path=$(realpath $0)
     local project_dir=$(dirname "$this_script_path")
+    pushd "$project_dir/e2e" >/dev/null
     uv --project "$project_dir/e2e" run py.test --no-header "$@"
+    popd >/dev/null
 }
 
 main "$@"
